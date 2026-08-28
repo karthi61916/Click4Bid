08d9ca820c5e16fc3a1017661a4822f2efc408d5
karthi61916 <karthikr@intellicloudtek.com>
2026-08-28 14:39:04 +0530

    Add routes for PropertyListings, Login, and Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Click4Bid from "./Click4Bid";
 import AboutUs from "./AboutUs";
-
+import PropertyListings from "./PropertyListings";
+import Login from "./Login";
+import Register from "./Register";
+ 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path="/" element={<Click4Bid />} />
-
         <Route path="/about" element={<AboutUs />} />
-
+        <Route path="/property-listing" element={<PropertyListings />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
+ 
 export default App;
